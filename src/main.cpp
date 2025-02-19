a7aefd1bd26f2901df067a9af16587f443862fb8
mrodz <79176075+mrodz@users.noreply.github.com>
2025-02-18 18:21:27 -0800

    finished code upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 #include <pico/sleep.h>
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
 const int BUTTON_PIN = 12;
 const int RELAY_PIN = 0;
-
-int buttonState = 0;
+const int LCD_I2C_ADDRESS = 0x27;
+const int LCD_COLUMNS = 16;
+const int LCD_ROWS = 2;
+const int SERIAL_BAUD_RATE = 4800;
 
 void setup_button()
 {
@@ -27,26 +29,25 @@ void relay_off()
   digitalWrite(RELAY_PIN, LOW);
 }
 
-void wake_up()
-{
-  // todo
-}
+static LiquidCrystal_I2C lcd(LCD_I2C_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
-const int rs = 16, en = 17, d4 = 18, d5 = 19, d6 = 20, d7 = 21;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+void setup_lcd()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
 
 void setup()
 {
-  Serial.begin(4800);
+  Serial.begin(SERIAL_BAUD_RATE);
   setup_button();
   setup_relay();
 }
 
-volatile byte relayState = LOW;
-long lastDebounceTime = 0;
-long debounceDelay = 10000;
-
-bool pushed = false;
+static volatile byte relayState = LOW;
+static volatile int buttonState = 0;
+static volatile bool pushed = false;
 
 bool await_loop_button()
 {
@@ -62,51 +63,96 @@ bool await_loop_button()
   }
 }
 
-const char *lovable_things[] = {
+const char *lovable_things[50] = {
+    "Your unwavering loyalty",
+    "You put in constant effort",
+    "You know how to cheer me up",
+    "You're buff as heck",
     "Your smile",
-    "Your hair",
-    "Your humor",
-    "Your hugs",
-    "Your eyes",
     "Your face",
     "Your nose",
     "How you comfort me",
     "Your stuffed animals",
+    "Your forgiveness",
+    "Your empathy",
+    "Your candles",
+    "The hand-written letter",
+    "Eating dinners together",
+    "Your joy when I give you flowers",
+    "Snoopy",
+    "Lamby",
+    "Mooteo",
+    "Bartholomew",
+    "Sushi dates",
+    "That you know how to code",
+    "That you're driven",
+    "Your hair",
+    "Your humor",
+    "Our study dates",
+    "Watching movies by your side",
+    "Giving head scratches",
+    "Receiving head scratches",
+    "Your massages are to die for",
+    "You have amazing style",
+    "Your music taste is great",
+    "You're gorgeous without makeup",
+    "... and even prettier with it",
+    "Your friends are pretty lit",
+    "You know how to have fun",
+    "Perfect balance of nerd + hottie",
+    "Your hugs",
+    "Your eyes",
+    "You give me butterflies 24/7",
+    "You're generous",
+    "I won the lotto to be your bf",
+    "Brazilian Baddie",
+    "You wait for me every day @ CCHS",
+    "We say hi every nutrition",
+    "Your gifts are meaningful",
+    "You're my tanning buddy",
+    "We can be biggies together",
+    "You've supported me at my lowest",
+    "I love talking to you",
+    "You make time for me",
 };
 
-void split_str(const char *src, char *dst1, char *dst2) {
+void split_str(const char *src, char *dst1, char *dst2)
+{
   memset(dst1, 0, 17);
   memset(dst2, 0, 17);
 
   int len = strlen(src);
 
-  if (len <= 16) {
-      strncpy(dst1, src, len);
-      dst1[len] = '\0';
-      return;
+  if (len <= 16)
+  {
+    strncpy(dst1, src, len);
+    dst1[len] = '\0';
+    return;
   }
 
   int split_pos = 16;
-  while (split_pos > 0 && !isspace(src[split_pos])) {
-      split_pos--;
+  while (split_pos > 0 && !isspace(src[split_pos]))
+  {
+    split_pos--;
   }
 
-  if (split_pos == 0) {
-      split_pos = 16;
+  if (split_pos == 0)
+  {
+    split_pos = 16;
   }
 
   strncpy(dst1, src, split_pos);
   dst1[split_pos] = '\0';
 
   const char *remaining = src + split_pos;
-  while (*remaining && isspace(*remaining)) {
-      remaining++;
+  while (*remaining && isspace(*remaining))
+  {
+    remaining++;
   }
 
   strncpy(dst2, remaining, 16);
   dst2[16] = '\0';
 }
-
 
 char lcd_lines[2][17];
 
@@ -118,11 +164,107 @@ void loop()
   setup_button();
   setup_relay();
 
+  relay_on();
+  setup_lcd();
+
   Serial.println("Wake up!");
 
-  relay_on();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("It's Valentine's");
+  lcd.setCursor(0, 1);
+  lcd.print("Day! I love you");
 
-  lcd.begin(16, 2);
+  delay(5000);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("In fact, I love");
+  lcd.setCursor(0, 1);
+  lcd.print("you so much...");
+
+  pushed = false;
+
+  for (int i = 0; i < 1200; i++)
+  {
+    buttonState = digitalRead(BUTTON_PIN);
+
+    if (buttonState == HIGH)
+    {
+      if (!pushed)
+      {
+        pushed = true;
+        break; // back to sleep
+      }
+    }
+    else
+    {
+      if (pushed)
+      {
+        pushed = false;
+      }
+    }
+
+    delay(5);
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("and I want to");
+  lcd.setCursor(0, 1);
+  lcd.print("share 50 things");
+
+  for (int i = 0; i < 1200; i++)
+  {
+    buttonState = digitalRead(BUTTON_PIN);
+
+    if (buttonState == HIGH)
+    {
+      if (!pushed)
+      {
+        pushed = true;
+        break; // back to sleep
+      }
+    }
+    else
+    {
+      if (pushed)
+      {
+        pushed = false;
+      }
+    }
+
+    delay(5);
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("I love about you");
+  lcd.setCursor(0, 1);
+  lcd.print("    enjoy <3");
+
+  for (int i = 0; i < 1200; i++)
+  {
+    buttonState = digitalRead(BUTTON_PIN);
+
+    if (buttonState == HIGH)
+    {
+      if (!pushed)
+      {
+        pushed = true;
+        break; // back to sleep
+      }
+    }
+    else
+    {
+      if (pushed)
+      {
+        pushed = false;
+      }
+    }
+
+    delay(5);
+  }
 
   for (auto &lovable_thing : lovable_things)
   {
@@ -158,6 +300,14 @@ void loop()
       delay(5);
     }
   }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Thank you for");
+  lcd.setCursor(0, 1);
+  lcd.print("everything <3");
+
+  delay(10000);
 
   relay_off();
 }
